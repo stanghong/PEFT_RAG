8c7178698a99c65fe3bffeeae6bce0418dcbb45d
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-11-30 13:18:51 -0800

    read customer jsons

--- utils/json2pdf.py
+++ utils/json2pdf.py
@@ -8,6 +8,12 @@ import os
 import glob
 from fpdf import FPDF
 # %%
+def clean_text(text):
+    '''
+    Replace Non-Latin Characters
+    '''
+    return text.encode('latin-1', 'replace').decode('latin-1')
+
 def json_to_pdf(json_file_path, output_folder):
     file_name = os.path.splitext(os.path.basename(json_file_path))[0]
     pdf_file_path = os.path.join(output_folder, f"{file_name}.pdf")
@@ -18,20 +24,25 @@ def json_to_pdf(json_file_path, output_folder):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
-    pdf.multi_cell(0, 10, content[file_name])
+
+    for key, value in content.items():
+        text = f"{key}: {value}"
+        # pdf.multi_cell(0, 10, text) #content[file_name]
+        pdf.multi_cell(0, 10, clean_text(text))
 
     os.makedirs(output_folder, exist_ok=True)
     pdf.output(pdf_file_path)
 
 def process_jsons_in_folder(input_folder, output_folder):
+    print(f'{input_folder}')
     for json_file in glob.glob(os.path.join(input_folder, '*.json')):
         print(f"Processing {json_file}")
 
         json_to_pdf(json_file, output_folder)
 # %%
 # Example usage
-input_folder = './output_json_files'
-output_folder = './output_pdf_files'
+input_folder = './data_json'
+output_folder = './output_pdfs'
 process_jsons_in_folder(input_folder, output_folder)
 
 print(f"PDF files saved in {output_folder}")
